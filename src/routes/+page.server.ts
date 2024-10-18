809fbfcf6c8b0c808b0a0a667044e28eea3ac789
Sean Johnston <sean.c.johnston@gmail.com>
2024-10-18 14:24:16 +0100

    Styling tweaks

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,11 +3,11 @@ import type { Actions, PageServerLoad } from './$types';
 const chapters = [
 	{
 		id: "ulid1",
-		text: "Once upon a test, there was some text here."
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
 	},
 	{
 		id: "ulid2",
-		text: "Then the text was more and it tested."
+		text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
 	},
 	{
 		id: "ulid3",
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,21 +8,20 @@
 
 <div class="container mx-auto w-dvw h-dvh">
 	<div class="flex flex-col h-full py-4 items-center">
-		<div class="relative h-3/5 w-3/5">
-			<div class="flex flex-col w-full h-full overflow-scroll">
+		<div class="relative h-3/5 w-5/6">
+			<div class="flex flex-col items-center w-full h-full overflow-scroll py-20">
 				{#each data.chapters as chapter}
-					<div class="card bg-secondary p-2 my-4 w-full text-center">
+					<div class="card bg-secondary p-4 my-6 text-center prose">
 						<p>{chapter.text}</p>
 					</div>
 				{/each}
 			</div>
+			<div class="bg-gradient-to-b from-base-100 to-transparent absolute top-0 w-full py-16 click-through"></div>
 			<div class="bg-gradient-to-t from-base-100 to-transparent absolute bottom-0 w-full py-16 click-through"></div>
 		</div>
 
-		<br />
-
 		<form action="?/add" method="post" use:enhance>
-			<div class="flex flex-col">
+			<div class="flex flex-col mt-8">
 				{#each data.questions as question, i}
 					<button class="btn my-2" type="submit" value={i}>{question}</button>
 				{/each}
