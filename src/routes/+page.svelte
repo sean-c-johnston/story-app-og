106f1300d55be7c2d90e502666365669493651a2
Sean Johnston <sean.c.johnston@gmail.com>
2024-10-18 11:59:16 +0100

    Add scrollbar back in, for accessibility purposes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 <div class="container mx-auto w-dvw h-dvh">
 	<div class="flex flex-col h-full py-4 items-center">
 		<div class="relative h-3/5 w-3/5">
-			<div class="flex flex-col w-full h-full overflow-scroll no-scrollbar">
+			<div class="flex flex-col w-full h-full overflow-scroll">
 				{#each data.chapters as chapter}
 					<div class="card bg-secondary p-2 my-4 w-full text-center">
 						<p>{chapter.text}</p>
